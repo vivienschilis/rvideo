d4950bd2632c5d712a267597ccd3316df807fff6
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2009-02-17 10:38:54 -0500

    include rvideo directory in require statements in lib/rvideo.rb

--- lib/rvideo.rb
+++ lib/rvideo.rb
@@ -1,26 +1,26 @@
-$LOAD_PATH.unshift File.dirname(__FILE__) + '/rvideo'
+$LOAD_PATH.unshift File.dirname(__FILE__)
 
 # core extensions
-require 'float'
-require 'string'
+require 'rvideo/float'
+require 'rvideo/string'
 
 # gems
 require 'rubygems'
 require 'active_support'
 
 # rvideo
-require 'inspector'
-require 'errors'
-require 'transcoder'
-require 'tools/abstract_tool'
-require 'tools/ffmpeg'
-require 'tools/mencoder'
-require 'tools/flvtool2'
-require 'tools/mp4box'
-require 'tools/mplayer'
-require 'tools/mp4creator'
-require 'tools/ffmpeg2theora'
-require 'tools/yamdi'
+require 'rvideo/inspector'
+require 'rvideo/errors'
+require 'rvideo/transcoder'
+require 'rvideo/tools/abstract_tool'
+require 'rvideo/tools/ffmpeg'
+require 'rvideo/tools/mencoder'
+require 'rvideo/tools/flvtool2'
+require 'rvideo/tools/mp4box'
+require 'rvideo/tools/mplayer'
+require 'rvideo/tools/mp4creator'
+require 'rvideo/tools/ffmpeg2theora'
+require 'rvideo/tools/yamdi'
 
-TEMP_PATH      = File.expand_path(File.dirname(__FILE__) + '/../tmp')
-REPORT_PATH    = File.expand_path(File.dirname(__FILE__) + '/../report')
+TEMP_PATH   = File.expand_path(File.dirname(__FILE__) + '/../tmp')
+REPORT_PATH = File.expand_path(File.dirname(__FILE__) + '/../report')
