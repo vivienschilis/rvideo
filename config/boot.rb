56eff1e25abbaa300c39788f87f5e55bc9b5ccc8
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2009-02-19 17:17:40 -0500

    setup full rvideo with config/boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,21 +1,6 @@
 # I don't know what this file was intended for originally, but I've been 
 # using it to setup RVideo to play with in an IRB session. - Seth
 
-$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
-
-require 'rvideo/inspector'
-require 'rvideo/tools/abstract_tool'
-require 'rvideo/tools/ffmpeg'
-require 'rvideo/tools/flvtool2'
-require 'rvideo/errors'
-require 'rvideo/transcoder'
-require 'yaml'
-require 'rubygems'
-require 'active_support'
-
-TEMP_PATH = File.expand_path(File.dirname(__FILE__) + '/../tmp')
-FIXTURE_PATH = File.expand_path(File.dirname(__FILE__) + '/../spec/fixtures')
-TEST_FILE_PATH = File.expand_path(File.dirname(__FILE__) + '/../spec/files')
-REPORT_PATH = File.expand_path(File.dirname(__FILE__) + '/../report')
-
+$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__),'..', 'lib'))
+require 'rvideo'
 include RVideo
