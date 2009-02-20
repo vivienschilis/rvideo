725f0aeb97fc2d98cb927c92650b6c92cd622a96
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2009-02-20 11:08:39 -0500

    some useful constants for ffmpeg tool

--- lib/rvideo/tools/ffmpeg.rb
+++ lib/rvideo/tools/ffmpeg.rb
@@ -1,6 +1,40 @@
 module RVideo
   module Tools
     class Ffmpeg
+      RESOLUTION_ABBREVIATIONS = {
+        "sqcif"  => "128x96",
+        "qcif"   => "176x144",
+        "cif"    => "352x288",
+        "4cif"   => "704x576",
+        "qqvga"  => "160x120",
+        "qvga"   => "320x240",
+        "vga"    => "640x480",
+        "svga"   => "800x600",
+        "xga"    => "1024x768",
+        "uxga"   => "1600x1200",
+        "qxga"   => "2048x1536",
+        "sxga"   => "1280x1024",
+        "qsxga"  => "2560x2048",
+        "hsxga"  => "5120x4096",
+        "wvga"   => "852x480",
+        "wxga"   => "1366x768",
+        "wsxga"  => "1600x1024",
+        "wuxga"  => "1920x1200",
+        "woxga"  => "2560x1600",
+        "wqsxga" => "3200x2048",
+        "wquxga" => "3840x2400",
+        "whsxga" => "6400x4096",
+        "whuxga" => "7680x4800",
+        "cga"    => "320x200",
+        "ega"    => "640x350",
+        "hd480"  => "852x480",
+        "hd720"  => "1280x720",
+        "hd1080" => "1920x1080"
+      }
+      
+      VALID_ASPECT_STRINGS = ["4:3", "16:9"]
+      VALID_ASPECT_FLOATS  = [1.3333, 1.7777]
+      
       # The flag used to set video bitrate has apparently changed between 
       # different ffmpeg versions. In the latest builds -b is used. 
       # In older builds it was -v which is now used to set verbosity of logging.
