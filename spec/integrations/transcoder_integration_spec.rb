53958491ddb94eccc9cce1c113479f5c6f7ca593
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2009-03-04 17:59:06 -0500

    so here is that currently poor FrameCapturer coverage i mentioned

--- spec/integrations/transcoder_integration_spec.rb
+++ spec/integrations/transcoder_integration_spec.rb
@@ -26,28 +26,4 @@ module RVideo
     # end
 
   end
-  
-  describe "When taking screenshots" do
-    after do
-      
-    end
-    
-    it "should create a screenshot with a default output path" do
-      output_file = "#{TEMP_PATH}/kites-10p.jpg"
-      FileTest.exist?(output_file).should_not be_true
-      file = Inspector.new(:file => spec_file("kites.mp4"))
-      file.capture_frame("10%").should == output_file
-      FileTest.exist?(output_file).should be_true
-      FileUtils.rm_f(output_file)
-    end
-    
-    it "should create a screenshot with a custom output path" do
-      output_file = spec_file("kites-10p.jpg")
-      FileTest.exist?(output_file).should be_false
-      file = Inspector.new(:file => spec_file("kites.mp4"))
-      file.capture_frame("10%", output_file).should == output_file
-      FileTest.exist?(output_file).should be_true
-      FileUtils.rm_f(output_file)
-    end
-  end
 end
