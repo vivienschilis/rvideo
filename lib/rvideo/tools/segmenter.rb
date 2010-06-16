0335867696482ad693b7e78fa85ed21c069fd894
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-06-16 15:31:57 +0100

    Segmenter raises when parsing error results

--- lib/rvideo/tools/segmenter.rb
+++ lib/rvideo/tools/segmenter.rb
@@ -12,6 +12,14 @@ module RVideo
       private
       
       def parse_result(result)
+        if m = /Could not write mpegts header to first output file/.match(result)
+          raise TranscoderError::InvalidFile
+        end
+        
+        if m = /Usage: segmenter <input MPEG-TS file> <segment duration in seconds> <output MPEG-TS file prefix> <output m3u8 index file> <http prefix>/.match(result)
+          raise TranscoderError::InvalidCommand, "Usage: segmenter <input MPEG-TS file> <segment duration in seconds> <output MPEG-TS file prefix> <output m3u8 index file> <http prefix>"
+        end
+        
         # TODO: parse segmenter results
         return true
       end
