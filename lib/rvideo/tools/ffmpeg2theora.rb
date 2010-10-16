a55f487ada0f5f3a0e7acfa8a88732b5c6394aed
Vivien Schilis <vivien.schilis@gmail.com>
2010-10-16 01:02:35 +0100

    parse ffmpeg2theora's progress

--- lib/rvideo/tools/ffmpeg2theora.rb
+++ lib/rvideo/tools/ffmpeg2theora.rb
@@ -9,6 +9,36 @@ module RVideo
         'ffmpeg2theora'
       end
 
+      def do_execute_with_progress(command, &block)
+        @raw_result = ''
+        duration = 0
+        CommandExecutor::execute_with_block(command, "\r") do |line|
+          progress, duration = parse_progress(line, duration)
+          block.call(progress) if block && progress
+          @raw_result += line.to_s + "\r"
+        end
+      end
+      
+      def execute_with_progress(&block)
+        RVideo.logger.info("\nExecuting Command: #{@command}\n")
+        do_execute_with_progress(@command, &block)
+      rescue RVideo::CommandExecutor::ProcessHungError
+        raise TranscoderError, "Transcoder hung."
+      end
+      
+      def parse_progress(line, duration)
+        if line =~ /Duration: (\d{2}):(\d{2}):(\d{2}).(\d{1})/
+          duration = (($1.to_i * 60 + $2.to_i) * 60 + $3.to_i) * 10 + $4.to_i
+        end
+
+        if line =~ /(\d{1,2}):(\d{2}):(\d{2})\.(\d)\d audio: \d+kbps video: \d+kbps/
+          current_time = (($1.to_i * 60 + $2.to_i) * 60 + $3.to_i) * 10 + $4.to_i
+        end
+        
+        progress = (current_time*100/duration) rescue nil
+        return [progress, duration]
+      end
+
       def format_video_quality(params={})
         bitrate = params[:video_bit_rate].blank? ? nil : params[:video_bit_rate]
         factor = (params[:scale][:width].to_f * params[:scale][:height].to_f * params[:fps].to_f)
